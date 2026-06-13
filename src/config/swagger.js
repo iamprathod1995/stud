1b957bf9f3d081963837b2d129ea2efdadb0f8ee
Pankaj Rathod <iamprathod1995@gmail.com>
2026-06-13 20:35:20 +0530

    Staff Leave Done Change Version

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -5,7 +5,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "RBAC API",
-      version: "1.0.2",
+      version: "1.0.3",
       description: "Role Permission Management API"
     },
     
