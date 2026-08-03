518250525efafc78989f036a2ed0730ad3319163
Pankaj Rathod <iamprathod1995@gmail.com>
2026-08-03 13:17:22 +0530

    code push

--- src/server.js
+++ src/server.js
@@ -1,24 +1,35 @@
-import app from './app.js';
 import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app.js';
 import { logger } from './utils/logger.js';
 import { checkDatabaseConnection } from './config/db.js';
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3005;
 const HOST = '0.0.0.0';
 
-  const dbConnected = await checkDatabaseConnection();
+async function startServer() {
+  try {
+    const dbConnected = await checkDatabaseConnection();
 
-  if (!dbConnected) {
-    logger.error('Database connection failed. Server stopped.');
+    if (!dbConnected) {
+      logger.error('Database connection failed. Server stopped.');
+      process.exit(1);
+    }
+
+    app.listen(PORT, HOST, () => {
+      logger.info('=======================================================');
+      logger.info('VidyaSetu Node.js Backend Server Active');
+      logger.info(`URL: http://${HOST}:${PORT}`);
+      logger.info(`Swagger Docs: http://${HOST}:${PORT}/api-docs`);
+      logger.info(`Storage Mode: ${process.env.STORAGE_TYPE || 'local'}`);
+      logger.info('=======================================================');
+    });
+
+  } catch (err) {
+    logger.error(err);
     process.exit(1);
   }
-app.listen(PORT, HOST, () => {
-  logger.info(`=======================================================`);
-  logger.info(` VidyaSetu Node.js Backend Server Active `);
-  logger.info(` URL: http://localhost:${PORT}`);
-  logger.info(` Swagger Docs: http://localhost:${PORT}/api-docs`);
-  logger.info(` Storage Mode: ${process.env.STORAGE_TYPE || 'local'}`);
-  logger.info(`=======================================================`);
-});
+}
+
+startServer();
