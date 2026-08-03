f0a1e320b4efef2f7e6889fd9b3f43929a2ff17e
Pankaj Rathod <iamprathod1995@gmail.com>
2026-08-03 12:25:36 +0530

    code change main file

--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ import { logger } from './utils/logger.js';
 
 dotenv.config();
 
-const PORT = process.env.APP_URL || 3005;
+const PORT = process.env.PORT || 3005;
 const HOST = '0.0.0.0';
+
 app.listen(PORT, HOST, () => {
   logger.info(`=======================================================`);
   logger.info(` VidyaSetu Node.js Backend Server Active `);
-  logger.info(` URL: ${APP_URL}`);
-  logger.info(` Swagger Docs: ${APP_URL}/api-docs`);
+  logger.info(` URL: http://localhost:${PORT}`);
+  logger.info(` Swagger Docs: http://localhost:${PORT}/api-docs`);
   logger.info(` Storage Mode: ${process.env.STORAGE_TYPE || 'local'}`);
   logger.info(`=======================================================`);
 });
